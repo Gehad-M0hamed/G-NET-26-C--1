7e488999b9a172e63ba047e912e4de04c0c05ba8
Gehad-M0hamed <ge73d2562005@gmail.com>
2026-03-09 00:35:50 +0200

    Question 1

--- G-NET-26-C#-1/Program.cs
+++ G-NET-26-C#-1/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace G_NET_26_C__1
+﻿using static System.Net.WebRequestMethods;
+
+namespace G_NET_26_C__1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question 1: Regions
+            // ══════════════════════════════════════════════════════════════════════
+            // QUESTION 2: REGIONS
+            // ══════════════════════════════════════════════════════════════════════
+            //
+            // Q: What is the purpose of #region and #endregion directives in C#? 
+            //-- Organize code into groups.
+            // 
+            //    How do they help in code organization?
+             //--making large code files easier to navigate and maintain.
+            //
+            // ══════════════════════════════════════════════════════════════════════
+
+            //Nested Region Example
+
+            #endregion
+
+
+
         }
     }
 }
